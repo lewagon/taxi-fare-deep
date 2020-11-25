076744624b37604778f1fc3ac512ac14ec5abce1
Bruno Lajoie <brunolajoie@gmail.com>
2020-11-25 16:01:16 +0100

    add regularization to NNet

--- taxifare_deep/network.py
+++ taxifare_deep/network.py
@@ -1,14 +1,20 @@
 from tensorflow.keras import layers, Sequential
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import regularizers
+
 
 class Network():
 
     def __init__(self, input_dim):
         self.input_dim = input_dim
+        reg = regularizers.l1_l2(l1=0.005, l2=0.0005)
         model = Sequential()
-        model.add(layers.Dense(100, input_dim=self.input_dim, activation='relu'))
-        model.add(layers.Dense(50, activation='relu'))
-        model.add(layers.Dense(20, activation='relu'))
+        model.add(layers.Dense(200, input_dim=self.input_dim, activation='relu', kernel_regularizer=reg))
+        layers.Dropout(rate=0.2)
+        model.add(layers.Dense(100, activation='relu', kernel_regularizer=reg))
+        layers.Dropout(rate=0.2)
+        model.add(layers.Dense(20, activation='relu', kernel_regularizer=reg))
+        layers.Dropout(rate=0.2)
         model.add(layers.Dense(1, activation='linear'))
         self.model = model
 
@@ -22,6 +28,6 @@ class Network():
             monitor='val_loss', patience=5, verbose=0, restore_best_weights=True)
 
         history = self.model.fit(X, y, validation_split=0.3,
-                  epochs=400, batch_size=128, callbacks=[es],
+                  epochs=500, batch_size=64, callbacks=[es],
                   verbose=verbose, workers=-1, use_multiprocessing=True)
         return history

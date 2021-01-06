619859aa63c44e0b9952f8050c6e850fe88961c9
kevin robert <kevin.j.robert@gmail.com>
2021-01-06 11:39:55 +0100

    fix syntax setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='taxifare_deep',
-      version="1.0",
-      author="Bruno Lajoie",
-      author_email="bruno@lewagon.org",
-      description="""Packaged neural network-based predictor for the Kaggle's NY
+setup(
+    name='taxifare_deep',
+    version="1.0",
+    author="Bruno Lajoie",
+    author_email="bruno@lewagon.org",
+    description="""Packaged neural network-based predictor for the Kaggle's NY
        Taxi Fare challenge""",
-      install_requires=requirements,
-      packages=find_packages(),
-      test_suite='tests',
-      include_package_data=True,
-      scripts=['scripts/taxifare_deep-run'],
-      zip_safe=False)
+    install_requires=requirements,
+    packages=find_packages(),
+    test_suite='tests',
+    include_package_data=True,
+    scripts=['scripts/taxifare_deep-run'],
+    zip_safe=False
+)

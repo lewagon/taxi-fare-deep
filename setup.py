4e779ecd766aaf0dff284430afc509a160b5dcd7
kevin robert <kevin.j.robert@gmail.com>
2021-01-06 11:04:42 +0100

    add author cc @brunolajoie

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='taxifare_deep',
       version="1.0",
-      description="Project Description",
+      author="Bruno Lajoie",
+      author_email="bruno@lewagon.org",
+      description="""Packaged neural network-based predictor for the Kaggle's NY
+       Taxi Fare challenge""",
       install_requires=requirements,
       packages=find_packages(),
-      test_suite = 'tests',
-      # include_package_data: to install data from MANIFEST.in
+      test_suite='tests',
       include_package_data=True,
       scripts=['scripts/taxifare_deep-run'],
       zip_safe=False)
